b005e4c8cd95e723c706556611cbdc5d520a5fcd
Ayad Masud <ayad.r.masud@gmail.com>
2024-06-22 17:34:11 -0500

    change to mediapipe

--- model.py
+++ model.py
@@ -1,90 +1,81 @@
-import cv2 as cv
+import cv2
+import mediapipe as mp
 import numpy as np
-import matplotlib.pyplot as plt
 
-# weights of the tensorflow model to detect the points on human skeleton
-net = cv.dnn.readNetFromTensorflow('graph_opt.pb')
+# setting up mediapipe utils and pose functions. 
+mp_drawing = mp.solutions.drawing_utils
+mp_pose = mp.solutions.pose
 
-# body parts
-BODY_PARTS = { "Nose": 0, "Neck": 1, "RShoulder": 2, "RElbow": 3, "RWrist": 4,
-                   "LShoulder": 5, "LElbow": 6, "LWrist": 7, "RHip": 8, "RKnee": 9,
-                   "RAnkle": 10, "LHip": 11, "LKnee": 12, "LAnkle": 13, "REye": 14,
-                   "LEye": 15, "REar": 16, "LEar": 17, "Background": 18 }
+# Setting up camera/image feed
+cap = cv2.VideoCapture(0)
 
-POSE_PAIRS = [ ["Neck", "RShoulder"], ["Neck", "LShoulder"], ["RShoulder", "RElbow"],
-                   ["RElbow", "RWrist"], ["LShoulder", "LElbow"], ["LElbow", "LWrist"],
-                   ["Neck", "RHip"], ["RHip", "RKnee"], ["RKnee", "RAnkle"], ["Neck", "LHip"],
-                   ["LHip", "LKnee"], ["LKnee", "LAnkle"], ["Neck", "Nose"], ["Nose", "REye"],
-                   ["REye", "REar"], ["Nose", "LEye"], ["LEye", "LEar"] ]
 
-# reading image
-# img = cv.imread('testImg/tm.jpg')
-
-# converting to bgr to rgb 
-# conv_img = cv.cvtColor(img, cv.COLOR_BGR2RGB), not needed because swapRB flag is used later on
-
-# defining image width and height
-inWidth = 1920
-inHeight = 1080
-thr = 1
-
-# function just for a single image
-cap = cv.VideoCapture(0)
-cap.set(cv.CAP_PROP_FPS, 10)
-cap.set(3, 800)
-cap.set(4, 800)
-
-if not cap.isOpened():
-    cap = cv.VideoCapture(0)
-if not cap.isOpened():
-    raise IOError("Cannot open webcam")
-    
-while cv.waitKey(1) < 0:
-    hasFrame, frame = cap.read()
-    if not hasFrame:
-        cv.waitKey()
-        break
+## Setup mediapipe instance
+with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
+    while cap.isOpened():
+        ret, frame = cap.read()
         
-    frameWidth = frame.shape[1]
-    frameHeight = frame.shape[0]
-    net.setInput(cv.dnn.blobFromImage(frame, 1.0, (inWidth, inHeight), (127.5, 127.5, 127.5), swapRB=True, crop=False))
-    out = net.forward()
-    out = out[:, :19, :, :]
+        # Recolor image to RGB
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        image.flags.writeable = False
+      
+        # Make detection
+        results = pose.process(image)
     
-    assert(len(BODY_PARTS) == out.shape[1])
-    
-    points = []
-    
-    for i in range(len(BODY_PARTS)):
-        # Slice heatmap of corresponding body's part.
-        heatMap = out[0, i, :, :]
-
-        # Originally, we try to find all the local maximums. To simplify a sample
-        # we just find a global one. However only a single pose at the same time
-        # could be detected this way.
-        _, conf, _, point = cv.minMaxLoc(heatMap)
-        x = (frameWidth * point[0]) / out.shape[3]
-        y = (frameHeight * point[1]) / out.shape[2]
-
-        # Add a point if it's confidence is higher than threshold.
-        points.append((int(x), int(y)) if conf > thr else None)
-
-    for pair in POSE_PAIRS:
-        partFrom = pair[0]
-        partTo = pair[1]
-        assert(partFrom in BODY_PARTS)
-        assert(partTo in BODY_PARTS)
-
-        idFrom = BODY_PARTS[partFrom]
-        idTo = BODY_PARTS[partTo]
-
-        if points[idFrom] and points[idTo]:
-            cv.line(frame, points[idFrom], points[idTo], (0, 255, 0), 3)
-            cv.ellipse(frame, points[idFrom], (3, 3), 0, 0, 360, (0, 0, 255), cv.FILLED)
-            cv.ellipse(frame, points[idTo], (3, 3), 0, 0, 360, (0, 0, 255), cv.FILLED)
+        # Recolor back to BGR
+        image.flags.writeable = True
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
         
-    t, _ = net.getPerfProfile()
-    freq = cv.getTickFrequency() / 1000
-    cv.putText(frame, '%.2fms' % (t / freq), (10, 20), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0))
+        # Extract landmarks
+        try:
+            landmarks = results.pose_landmarks.landmark
+            
+            # Get coordinates
+            shoulder = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
+            elbow = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x,landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y]
+            wrist = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x,landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
+            
+            # Calculate angle
+            angle = angle(shoulder, elbow, wrist)
+            
+            # Visualize angle
+            cv2.putText(image, str(angle), 
+                           tuple(np.multiply(elbow, [640, 480]).astype(int)), 
+                           cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA
+                                )
+        except:
+            pass
+        
+        # Render detections
+        mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
+                                mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=2), 
+                                mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2) 
+                                 )               
+        
+        # show image
+        cv2.imshow('Mediapipe Feed', image)
 
-    cv.imshow('Pose estimation trial', frame)
+        # ending camera/image feed
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+    # Angle finder function
+    # Feed in 3 arguments 
+    def angle(a,b,c):
+        a = np.array(a) # First
+        b = np.array(b) # Mid
+        c = np.array(c) # End
+        
+        # endY - midY, endX - midX    -  firstY - midY, firstX - midX  
+        radians = np.arctan2(c[1]-b[1], c[0]-b[0]) - np.arctan2(a[1]-b[1], a[0]-b[0])
+        angle = np.abs(radians*180.0/np.pi)
+        
+        # because arm cant rotate 360 to normalize angle value.
+        if angle >180.0:
+            angle = 360-angle
+            
+        return angle 
